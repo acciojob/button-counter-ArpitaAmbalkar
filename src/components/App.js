2df81cde8e063c76ca0d3e57fc5298af2dad049e
Arpita <arpitaambalkar26@gmail.com>
2025-12-27 01:53:15 +0530

    Submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { useState } from "react";
 const App = () => {
+  let [count,update]=useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={()=>update(count+1)}>Click me</button>
     </div>
   )
 }
